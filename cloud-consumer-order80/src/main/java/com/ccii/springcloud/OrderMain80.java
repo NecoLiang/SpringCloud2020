3bcf30df10ddf920ad5c4e6bfa11783729072480
liangyutao <liangyutao>
2020-08-04 22:05:29 +0800

    1.消费者模块、订单模块搭建 2.提取公共实体Payment，配置pom文件

---
+++ cloud-consumer-order80/src/main/java/com/ccii/springcloud/OrderMain80.java
@@ -0,0 +1,16 @@
+package com.ccii.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liangyt
+ * @create 2020-08-04 20:39
+ */
+@SpringBootApplication
+public class OrderMain80 {
+    public static void main(String[] args){
+        SpringApplication.run(OrderMain80.class,args);
+    }
+
+}
